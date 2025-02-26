7b903d3668eef86e4849811c4b683cf74dc97455
Belal <0xbelalmhmd@gmail.com>
2025-02-26 09:42:45 +0200

    fix

---
+++ Core/Student.h
@@ -0,0 +1,16 @@
+//
+// Created by Belal on 26-Feb-25.
+//
+
+#ifndef STUDENT_H
+#define STUDENT_H
+
+
+
+class Student {
+
+};
+
+
+
+#endif //STUDENT_H
--- Global/Global.h
+++ Global/Global.h
@@ -7,6 +7,7 @@
 
 #include<iostream>
 #include "../Core/Admin.h"
+#include <fstream>
 using namespace std;
 
 #include <bits/stdc++.h>
@@ -18,5 +19,17 @@ const string Admin_File = "E:\\Computer Science\\Projects\\Educational Managemen
 const string ComputerScience = "E:\\Computer Science\\Projects\\Educational Management Sys\\DataSource\\ComputerScience\\";
 const string AllAdmin_File = "E:\\Computer Science\\Projects\\Educational Management System\\DataSource\\AllAdmin.txt";
 
-
+int GenerateID() {
+      fstream file("E:\\Computer Science\\Projects\\Educational Management System\\Global\\ID.txt",ios::in | ios::out);
+        string line;
+        int ID;
+        if(file.is_open()) {
+            while(getline(file,line)) {
+                ID = stoi(line);
+                cout<<to_string(ID+1);
+            }
+            file.close();
+        }
+    return ID;
+}
 #endif //GLOBAL_H
