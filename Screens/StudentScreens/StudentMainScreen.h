13382fe777ed819f3cfe64681aee1fac20ed9196
Belal <0xbelalmhmd@gmail.com>
2025-03-13 17:00:17 +0200

    add manage students screen

--- Screens/StudentScreens/StudentMainScreen.h
+++ Screens/StudentScreens/StudentMainScreen.h
@@ -11,8 +11,9 @@
 #include "../../lib/clsInputValidate.h"
 #include "../../Screens/StudentScreens/GetStudentIDScreen.h"
 #include "RegistrationScreen.h"
+#include "RegistredCoursesScreen.h"
 using namespace  std;
-enum eStudentScreens {Registeretion = 1, GetID = 2, Exit = 3};
+enum eStudentScreens {Registeretion = 1, RegistredCourses =2 ,GetID = 3, Exit = 4};
 class StudentMainScreen : public  Screen {
 private:
 
@@ -23,14 +24,19 @@ public:
         Screen::DrawScreen("Student Main Screen");
         cout<<"\n";
         cout<<"1. Registeretion "<<endl;
-        cout<<"2. Get ID By National ID"<<endl;
-        cout<<"3. Exit"<<endl;
+        cout<<"2. Registred Courses"<<endl;
+        cout<<"3. Get ID By National ID"<<endl;
+        cout<<"4. Exit"<<endl;
 
-        switch (short ANS = clsInputValidate::ReadShortNumberBetween(1,3)) {
+        switch (short ANS = clsInputValidate::ReadShortNumberBetween(1,4)) {
             case Registeretion:
                 //cout<<"Registeretion screen will be here....\n";
                     RegistrationScreen::Show();
                 Show();
+            break;
+            case RegistredCourses:
+                RegistredCoursesScreen::Show();
+                 Show();
             break;
             case GetID:
                // cout<<"Get ID screen will be here....\n";
