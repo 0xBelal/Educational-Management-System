765cf70a521beee44c3d5314a2f8cd6310827045
Belal <0xbelalmhmd@gmail.com>
2025-03-15 01:32:01 +0200

    added print registered courses

--- Core/Student.h
+++ Core/Student.h
@@ -229,6 +229,53 @@ public:
     }
 };
 
+static void PrintHeader() {
+    std::cout << std::left << std::setw(11) << "Course Code"
+              << " | " << std::setw(26) << "Course Name"
+              << " | " << std::setw(10) << "Credit Hour"
+              << " | " << std::setw(10) << "Student Mark"
+
+              << std::endl;
+    std::cout << std::string(70, '_') << std::endl;
+}
+
+static void PrintCourse(const Course &course) {
+    std::cout << std::left << std::setw(11) << course.getCourseCode()
+              << " | " << std::setw(26) << course.getName()
+              << " | " << std::setw(10) << course.getCreditHours()
+              << " | " << std::setw(10)  << course.getStudentMark();
+
+    /*std::vector<std::string> prerequisites = course.getPrerequisitesCourses();
+    if (!prerequisites.empty()) {
+        for (size_t i = 0; i < prerequisites.size(); i++) {
+            std::cout << prerequisites[i];
+            if (i != prerequisites.size() - 1) std::cout << " ";
+        }
+    }*/
+
+    std::cout << std::endl;
+}
+
+
+static void PrintRegistredCourses(Student &student) {
+    std::vector<Course> vEnrolledCourses = student.getEnrolledCourses();
+    // CS102 <-> Programming 2 <-> 3 <-> 0.000000 <-> CS101
+
+    std::cout << "\n\n";
+    PrintHeader();
+    int TotalHoures = 0, TotalCourses = 0;
+    for (Course &x : vEnrolledCourses) {
+        TotalCourses++;
+        TotalHoures+=x.getCreditHours();
+        PrintCourse(x);
+    }
+
+    std::cout << std::string(70, '_') << std::endl;
+    cout<<"Total Courses: "<<TotalCourses<<"             Total Hours: "<<TotalHoures<<endl;
+    std::cout << std::string(70, '_') << std::endl;
+
+
+}
 
 
 #endif //STUDENT_H
